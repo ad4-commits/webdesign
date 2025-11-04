67da5b3b952d849e5c3f15cd75e54562056f4acb
Ad.e <adedoc8@gmail.com>
2025-11-03 23:47:22 -0600

    Add ParentNode class with recursive to_html method and comprehensive tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -36,3 +36,28 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+        
+        if self.children is None:
+            raise ValueError("ParentNode must have children")
+        
+        if len(self.children) == 0:
+            raise ValueError("ParentNode must have at least one child")
+        
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        
+        props_html = self.props_to_html()
+        return f"<{self.tag}{props_html}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode(tag={self.tag}, children={self.children}, props={self.props})"
--- src/main.py
+++ src/main.py
@@ -1,29 +1,49 @@
 from textnode import TextNode, TextType
-from htmlnode import LeafNode
+from htmlnode import LeafNode, ParentNode
 
 def main():
     # Original TextNode demonstration
     text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print("TextNode:", text_node)
     
-    # New LeafNode demonstrations
-    print("\nLeafNode Examples:")
+    # New ParentNode demonstrations
+    print("\nParentNode Examples:")
     
-    # Paragraph
-    paragraph = LeafNode("p", "This is a paragraph of text.")
-    print("Paragraph:", paragraph.to_html())
+    # Simple parent with children
+    simple_parent = ParentNode(
+        "p",
+        [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ],
+    )
+    print("Simple Parent:", simple_parent.to_html())
     
-    # Link with props
-    link = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-    print("Link:", link.to_html())
-    
-    # Raw text (no tag)
-    raw_text = LeafNode(None, "This is just raw text")
-    print("Raw text:", raw_text.to_html())
-    
-    # Heading with class
-    heading = LeafNode("h1", "Welcome to my site", {"class": "title"})
-    print("Heading:", heading.to_html())
+    # Nested parent structure
+    nested_parent = ParentNode(
+        "div",
+        [
+            LeafNode("h1", "Main Title"),
+            ParentNode(
+                "ul",
+                [
+                    LeafNode("li", "Item 1"),
+                    LeafNode("li", "Item 2"),
+                    ParentNode(
+                        "li",
+                        [
+                            LeafNode("b", "Important"),
+                            LeafNode(None, " item")
+                        ]
+                    )
+                ]
+            )
+        ],
+        {"class": "container"}
+    )
+    print("Nested Parent:", nested_parent.to_html())
 
 if __name__ == "__main__":
     main()
